601df9abfc2f8b352f419843af232aaab5c74599
annassetiawan <setiawanannas123@gmail.com>
2026-05-05 18:35:18 +0700

    feat: add type definitions and validation for page entry in Next.js application

--- .next/static/webpack/webpack.cc525f51128d313d.hot-update.js
+++ .next/static/webpack/webpack.cc525f51128d313d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7fd7232e779afccb"; }
+/******/ 	__webpack_require__.h = function() { return "9624af6552d65665"; }
 /******/ }();
 /******/ 
 /******/ }
